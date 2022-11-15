6f2a787cbdceec678dd9f5cadcbb21857c372481
shilpa nair <shilpa.sathya@gmail.com>
2022-11-15 15:25:51 -0600

    Andy's review comments

--- web/src/pages/viewEmployees.js
+++ web/src/pages/viewEmployees.js
@@ -7,15 +7,15 @@ import DataStore from "../util/DataStore";
  * Logic needed for the view playlist page of the website.
  */
 //Global variable to track the very first employee Id retrieved and the first page.
-var firstEmpId = '';
+
 class ViewEmployees extends BindingClass {
 
     constructor() {
         super();
-        this.bindClassMethods(['clientLoaded', 'mount', 'displayEmployeesOnPage', 'generateTable', 'next', 'previous',  'addEmployee'], this);
+        this.bindClassMethods(['clientLoaded', 'mount', 'displayEmployeesOnPage', 'generateTable', 'next', 'previous'  ], this);
         this.dataStore = new DataStore();
+        this.dataStore.addChangeListener(this.displayEmployeesOnPage);
         this.header = new Header(this.dataStore);
-        console.log("viewEmployees constructor");
     }
 
  /**
@@ -29,10 +29,7 @@ class ViewEmployees extends BindingClass {
         //Get all employees API
         const employees = await this.client.getAllEmployees(0, true);
         this.dataStore.set('employees', employees);
-        firstEmpId = employees[0].employeeId;
-        console.log("This is my first employee Id from the table: ", firstEmpId )
-        console.log("In ClientLoaded method: Employees values: ",  employees);
-        this.displayEmployeesOnPage();
+        this.dataStore.set('firstEmpId', employees[0].employeeId);
 
     }
 
@@ -84,20 +81,19 @@ class ViewEmployees extends BindingClass {
      */
     displayEmployeesOnPage() {
         const employees = this.dataStore.get('employees');
-        console.log("In DisplayEmployeesMethod: Employees values: ",  employees);
-        
+
         if (!employees) {
             return;
         }
             let table = document.querySelector("table");
-            let data = Object.keys(employees);
 
+            //Flush the table first
             var tableHeaderRowCount = 1;
             var rowCount = table.rows.length;
             for (var i = tableHeaderRowCount; i < rowCount; i++) {
                 table.deleteRow(tableHeaderRowCount);
             }
-
+            //Generate table data with the new set of employees
             this.generateTable(table, employees);
             document.getElementById('employees').innerText = "";
 
@@ -109,7 +105,7 @@ class ViewEmployees extends BindingClass {
                 document.getElementById('next').style.background='#F5881F';
             }
 
-            if (employees[0].employeeId == firstEmpId) {
+            if (employees[0].employeeId ==  this.dataStore.get('firstEmpId')) {
                 document.getElementById('previous').disabled = true;
                 document.getElementById('previous').style.background='grey';
             }
@@ -119,7 +115,6 @@ class ViewEmployees extends BindingClass {
          const employees = this.dataStore.get('employees');
          const employeesNext = await this.client.getAllEmployees(employees[4].employeeId, true);
          this.dataStore.set('employees', employeesNext);
-         this.displayEmployeesOnPage();
 
          document.getElementById('previous').disabled = false;
          document.getElementById('previous').style.background='#F5881F';
@@ -130,36 +125,9 @@ class ViewEmployees extends BindingClass {
      async previous() {
          const employees = this.dataStore.get('employees');
          const employeesPrev = await this.client.getAllEmployees(employees[0].employeeId, false);
-         //Reversing since DynamoDB withScanIndexForward retrieves data in the reversed order
-         employeesPrev.reverse();
          this.dataStore.set('employees', employeesPrev);
-         this.displayEmployeesOnPage();
      }
 
-
-
- /**
-     * Method to run when the add employee submit button is pressed. Call the EmployeeMgmtClient to add employee to the
-     * list.
-     */
-     //TODO: Shilpa I think this may need to be moved to the add_employee page js because of our UI design
-    async addEmployee() {
-        const playlist = this.dataStore.get('playlist');
-        if (playlist == null) {
-            return;
-        }
-
-//        document.getElementById('add-song').innerText = 'Adding...';
-//        const asin = document.getElementById('album-asin').value;
-//        const trackNumber = document.getElementById('track-number').value;
-//        const playlistId = playlist.id;
-
-        const songList = await this.client.addEmployee(playlistId, asin, trackNumber);
-        this.dataStore.set('songs', songList);
-
-        document.getElementById('add-song').innerText = 'Add Song';
-        document.getElementById("add-song-form").reset();
-    }
 }
 
 /**
