57268017456026705279482e99e8ae850e08ccac
shilpa nair <shilpa.sathya@gmail.com>
2022-11-15 14:39:45 -0600

    done with basic view employees, next and previous nav

--- web/src/pages/viewEmployees.js
+++ web/src/pages/viewEmployees.js
@@ -6,7 +6,10 @@ import DataStore from "../util/DataStore";
 /**
  * Logic needed for the view playlist page of the website.
  */
+//Global variable to track the very first employee Id retrieved and the first page.
+var firstEmpId = '';
 class ViewEmployees extends BindingClass {
+
     constructor() {
         super();
         this.bindClassMethods(['clientLoaded', 'mount', 'displayEmployeesOnPage', 'generateTable', 'next', 'previous',  'addEmployee'], this);
@@ -20,8 +23,14 @@ class ViewEmployees extends BindingClass {
      */
     async clientLoaded() {
         document.getElementById('employees').innerText = "(Loading employee list...)";
+        //Disable prev button on initial load
+        document.getElementById('previous').disabled = true;
+        document.getElementById('previous').style.background='grey';
+        //Get all employees API
         const employees = await this.client.getAllEmployees(0, true);
         this.dataStore.set('employees', employees);
+        firstEmpId = employees[0].employeeId;
+        console.log("This is my first employee Id from the table: ", firstEmpId )
         console.log("In ClientLoaded method: Employees values: ",  employees);
         this.displayEmployeesOnPage();
 
@@ -58,8 +67,14 @@ class ViewEmployees extends BindingClass {
         cell.appendChild(text);
 
         cell = row.insertCell();
+        // Create anchor element.
+        var a = document.createElement('a');
         text = document.createTextNode(element.email);
-        cell.appendChild(text);
+        // Append the text node to anchor element.
+        a.appendChild(text);
+        a.title = element.email
+        a.href = 'mailto:' + element.email;
+        cell.appendChild(a);
 
       }
     }
@@ -70,7 +85,7 @@ class ViewEmployees extends BindingClass {
     displayEmployeesOnPage() {
         const employees = this.dataStore.get('employees');
         console.log("In DisplayEmployeesMethod: Employees values: ",  employees);
-
+        
         if (!employees) {
             return;
         }
@@ -84,22 +99,17 @@ class ViewEmployees extends BindingClass {
             }
 
             this.generateTable(table, employees);
+            document.getElementById('employees').innerText = "";
 
-//        let employeeHtml = '';
-//        let employee;
-//        for (employee of employees) {
-//            employeeHtml += `
-//                <li class="employee">
-//                    <span class="department">${employee.deptName}</span>
-//                    <span class="firstname">${employee.firstName}</span>
-//                    <span class="lastname">${employee.lastName}</span>
-//                    <span class="email">${employee.email}</span>
-//
-//                </li>
-//            `;
-//        }
-//        document.getElementById('employees').innerHTML = employeeHtml;
+            if (employees.length < 5){
+                document.getElementById('next').disabled = true;
+                document.getElementById('next').style.background='grey';
+            }
 
+            if (employees[0].employeeId == firstEmpId) {
+                document.getElementById('previous').disabled = true;
+                document.getElementById('previous').style.background='grey';
+            }
  }
 
      async next() {
@@ -107,12 +117,18 @@ class ViewEmployees extends BindingClass {
          const employeesNext = await this.client.getAllEmployees(employees[4].employeeId, true);
          this.dataStore.set('employees', employeesNext);
          this.displayEmployeesOnPage();
+
+         document.getElementById('previous').disabled = false;
+         document.getElementById('previous').style.background='#F5881F';
+
      }
 
 
      async previous() {
          const employees = this.dataStore.get('employees');
          const employeesPrev = await this.client.getAllEmployees(employees[0].employeeId, false);
+         //Reversing since DynamoDB withScanIndexForward retrieves data in the reversed order
+         employeesPrev.reverse();
          this.dataStore.set('employees', employeesPrev);
          this.displayEmployeesOnPage();
      }
