7c75fbe99428b7e836eb5c02fa1d97035acdd14d
Josh Taylor <joshmtaylor2000@gmail.com>
2022-11-29 14:07:22 -0600

    cosmetic change to code

--- EmployeeContactServiceLambda/src/test/java/com/nashss/se/employeecontactservice/activity/CreateDepartmentActivityTest.java
+++ EmployeeContactServiceLambda/src/test/java/com/nashss/se/employeecontactservice/activity/CreateDepartmentActivityTest.java
@@ -59,10 +59,10 @@ public class CreateDepartmentActivityTest {
     @Test
     void handleRequest_invalidDepartmentName_throwsException() {
         // GIVEN
-        CreateDepartmentRequest request = CreateDepartmentRequest.
-                builder().
-                withDeptName("dep@rtment").
-                build();
+        CreateDepartmentRequest request = CreateDepartmentRequest
+                .builder()
+                .withDeptName("dep@rtment")
+                .build();
 
         // WHEN + THEN
         assertThrows(InvalidAttributeValueException.class, () -> createDepartmentActivity.handleRequest(request));
