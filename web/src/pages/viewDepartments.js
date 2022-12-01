bd0cfd781342f927c5415154e5dfe0f3541fd322
Surinh Siri <surinhjs@gmail.com>
2022-12-01 09:27:10 -0600

    added Department Page

--- web/src/pages/viewDepartments.js
+++ web/src/pages/viewDepartments.js
@@ -22,15 +22,10 @@ class ViewDepartments extends BindingClass {
      */
     async clientLoaded() {
         document.getElementById('departments').innerText = "(Loading employee list...)";
-        //Disable prev button on initial load
-//        document.getElementById('previous').disabled = true;
-//        document.getElementById('previous').style.background='grey';
-        //Get all employees API
+        //Get all departments API
         const departments = await this.client.getAllDepartments();
         this.dataStore.set('departments', departments);
-//        this.dataStore.set('veryFirstDeptId', employees[0].employeeId);
-//        this.dataStore.set('firstDeptId', employees[0].employeeId);
-        //await this.loadDeptDropDown();
+
     }
 
 
@@ -38,8 +33,6 @@ class ViewDepartments extends BindingClass {
      * Add the header to the page and load the EmployeeMgmtClientClient.
      */
     async mount() {
-//        document.getElementById('next').addEventListener('click', this.next);
-//        document.getElementById('previous').addEventListener('click', this.previous);
 
         this.header.addHeaderToPage();
         this.header.loadData();
@@ -54,10 +47,10 @@ class ViewDepartments extends BindingClass {
           for (let element of data) {
             let row = table.insertRow();
 
-//            row.addEventListener('click', async evt => {
-//                      console.log('The element that was clicked was ', element.employeeId);
-//                        window.location.href = `/view_employee.html?id=${element.employeeId}`;
-//                      });
+            row.addEventListener('click', async evt => {
+                      console.log('The element that was clicked was ', element.deptId);
+                        window.location.href = `/view_department.html?id=${element.deptId}`;
+                      });
 
 
             let cell = row.insertCell();
@@ -104,108 +97,6 @@ class ViewDepartments extends BindingClass {
         }
  }
 
-//     async next() {
-//         const employees = this.dataStore.get('employees');
-//
-//         const dept = document.getElementById('depts');
-//         const deptId = document.getElementById('depts').value;
-//         const deptName = dept.options[dept.selectedIndex].innerHTML;
-//
-//         if (deptId == 'ALL'){
-//            const employeesNext = await this.client.getAllEmployees(employees[19].employeeId, true);
-//            if (employeesNext.length !=0){
-//                document.getElementById('previous').disabled = false;
-//                document.getElementById('previous').style.background='#F5881F';
-//                this.dataStore.set('employees', employeesNext);
-//            }
-//             if (employeesNext.length < 20) {
-//                document.getElementById('next').disabled = true;
-//                document.getElementById('next').style.background='grey';
-//            }
-//
-//         } else {
-//
-//            const employeesInDept = await this.client.getAllEmployeesByDept(employees[19].employeeId, true, deptId);
-//            if (employeesInDept.length !=0){
-//               document.getElementById('previous').disabled = false;
-//               document.getElementById('previous').style.background='#F5881F';
-//               this.dataStore.set('employees', employeesInDept);
-//            }
-//             if (employeesInDept.length < 20)  {
-//               document.getElementById('next').disabled = true;
-//               document.getElementById('next').style.background='grey';
-//            }
-//
-//         }
-//
-//         this.dataStore.set('firstEmpId', employees[0].employeeId);
-//
-//     }
-//
-//
-//     async previous() {
-//         const employees = this.dataStore.get('employees');
-//
-//         const dept = document.getElementById('depts');
-//         const deptId = document.getElementById('depts').value;
-//         const deptName = dept.options[dept.selectedIndex].innerHTML;
-//
-//         if (deptId == 'ALL'){
-//            const employeesPrev = await this.client.getAllEmployees(employees[0].employeeId, false);
-//            this.dataStore.set('employees', employeesPrev);
-//            if (employeesPrev.length != 0 && employeesPrev[0].employeeId ==  this.dataStore.get('veryFirstEmpId')){
-//                document.getElementById('previous').disabled = true;
-//                document.getElementById('previous').style.background='grey';
-//            }
-//
-//         } else {
-//            const employeesInDept = await this.client.getAllEmployeesByDept(employees[0].employeeId, false, deptId);
-//            this.dataStore.set('employees', employeesInDept);
-//            if (employeesInDept.length != 0 && employeesInDept[0].employeeId ==  this.dataStore.get('veryFirstEmpIdOfDept')){
-//                document.getElementById('previous').disabled = true;
-//                document.getElementById('previous').style.background='grey';
-//            }
-//         }
-//         document.getElementById('next').disabled = false;
-//         document.getElementById('next').style.background='#F5881F';
-//         this.dataStore.set('firstEmpId', employees[0].employeeId);
-//     }
-//
-//      async deptChange() {
-//         //Disable prev button on initial load
-//         document.getElementById('previous').disabled = true;
-//         document.getElementById('previous').style.background='grey';
-//
-//         document.getElementById('next').disabled = false;
-//         document.getElementById('next').style.background='#F5881F';
-//
-//         const dept = document.getElementById('depts');
-//
-//         const deptId = document.getElementById('depts').value;
-//         const deptName = dept.options[dept.selectedIndex].innerHTML;
-//
-//         console.log("Department ID is: ", deptId);
-//         console.log("Department Name is: ", deptName);
-//
-//         if (deptId == 'ALL'){
-//            const employees = await this.client.getAllEmployees(0, true, deptId);
-//            this.dataStore.set('employees', employees);
-//            this.dataStore.set('veryFirstEmpIdOfDept', employees[0].employeeId);
-//            if (employees.length < 20) {
-//                document.getElementById('next').disabled = true;
-//                document.getElementById('next').style.background='grey';
-//            }
-//         } else {
-//            const employeesInDept = await this.client.getAllEmployeesByDept(0, true, deptId);
-//            this.dataStore.set('employees', employeesInDept);
-//            this.dataStore.set('veryFirstEmpIdOfDept', employeesInDept[0].employeeId);
-//             if (employeesInDept.length < 20) {
-//                 document.getElementById('next').disabled = true;
-//                 document.getElementById('next').style.background='grey';
-//             }
-//         }
-//      }
-//}
 }
 /**
  * Main method to run when the page contents have loaded.
